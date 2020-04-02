48ec704fc6b448cd0e31197d341a84a94f6cf3a5
marekksiazek <59536777+marekksiazek@users.noreply.github.com>
2020-04-02 21:38:14 +0200

    Add files via upload

---
+++ hide.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('*[data-animate]').addClass('hide').each(function () {
+        $(this).viewportChecker({
+            classToAdd: 'show animated ' + $(this).data('animate'),
+            classToRemove: 'hide',
+            offset: '20%'
+        });
+    });
+
+});
